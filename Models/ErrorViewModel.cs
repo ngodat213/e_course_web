2713b4e78c481529a8b0dfa5544882385329ff38
Hydra Coder <96688421+ngodat213@users.noreply.github.com>
2024-03-05 22:58:50 +0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace e_course_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
